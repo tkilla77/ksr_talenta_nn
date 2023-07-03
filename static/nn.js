b98bdc8140b9c9389ac9d081c204a5a1402fe711
Tom Hofmann <tom@scheidweg.net>
2023-07-03 12:08:15 +0200

    fix model passing

--- static/nn.js
+++ static/nn.js
@@ -54,7 +54,7 @@ async function predict(canvas, model, history) {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            label: 'mnist_best',
+            label: model,
             data: url,
         }),
     };
